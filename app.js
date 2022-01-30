8b8ddcb11c35d5a6f60e2c568a091038738194d6
Kyle <tan.kyleemmanuel@gmail.com>
2022-01-30 22:21:51 +0800

    working createcard function

--- app.js
+++ app.js
@@ -6,7 +6,14 @@ let myLibrary = [
     }
 ];
 
-const container = document.getElementById("container");
+
+
+const kyle = {
+    title: "kyle amazings stories",
+    author: "kyle",
+    read: false,
+};
+
 
 
 function Book(title, author, read) {
@@ -16,5 +23,57 @@ function Book(title, author, read) {
 }
 
 function addBookToLibrary(){
+    if(myLibrary.length !== 0){
+        myLibrary.forEach(element => {
+            
+        });
+    }
+}
 
+function hehe(){
+    createCard(kyle);
+}
+
+function createCard(obj){
+    const card = document.createElement("div");
+    card.className = "card";
+
+    const title = document.createElement("div");
+    title.className = "title";
+    title.textContent = "Title: "
+    const titleObj = document.createElement("div");
+    titleObj.className = "title";
+    titleObj.textContent = obj["title"];
+
+    const author = document.createElement("div");
+    author.className = "author";
+    author.textContent = "Author: "
+    const authorObj = document.createElement("div");
+    authorObj.className = "author";
+    authorObj.textContent = obj.author;
+
+    const toggles = document.createElement("div");
+    toggles.className = "toggles";
+    
+    const read = document.createElement("button");
+    read.className = "read";
+    read.textContent = "READ";
+
+    const trash = document.createElement("button");
+    
+    const trashIcon = document.createElement("i");
+    trashIcon.className = "fa fa-trash-o trash";
+
+    trash.appendChild(trashIcon);
+
+    toggles.appendChild(read);
+    toggles.appendChild(trash);
+    
+    card.appendChild(title);
+    card.appendChild(titleObj);
+    card.appendChild(author);
+    card.appendChild(authorObj);
+    card.appendChild(toggles);
+    const container = document.getElementById("container");
+    container.appendChild(card);
 }
